7f822637b7d2db482fb4d2f7c2dad6948ade7c34
Alejandro-Bravo2 <abravoc@iesrafaelalberti.es>
2025-10-11 15:41:51 +0200

    probando

--- tmp/updateReadme.py
+++ tmp/updateReadme.py
@@ -1,10 +1,11 @@
-
+from datetime import datetime
 
 with open("salidaEjecucion.txt", "r", encoding="utf-8") as archivo:
     contenido = archivo.read()
     lineas = contenido.split("\n")
     if ("> Task :test FAILED" in lineas):
-        print("FALLIDO")
+        archivo.write(f"Resultado del test, Fecha: ${datetime.now()}")
+        archivo.write(f"❌ Tests fallidos")
     else:
-        print("CORRECTOS")
-        
+        archivo.write(f"Resultado del test, Fecha: ${datetime.now()}")
+        archivo.write(f"✅ Tests correctos")        
