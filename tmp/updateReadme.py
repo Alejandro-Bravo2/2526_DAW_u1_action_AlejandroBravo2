913c1410f291bdedb9ea405b9712aa8d26f6d2b8
Alejandro-Bravo2 <abravoc@iesrafaelalberti.es>
2025-10-11 16:26:09 +0200

    probando

--- tmp/updateReadme.py
+++ tmp/updateReadme.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
-with open("salidaEjecucion.txt", "r", encoding="utf-8") as archivo:
+with open("../../tmp/salidaEjecucion.txt", "r", encoding="utf-8") as archivo:
     contenido = archivo.read()
     lineas = contenido.split("\n")
     if ("> Task :test FAILED" in lineas):
-        with open("../README.md", "w", encoding="utf-8") as readme:
+        with open("../../README.md", "w", encoding="utf-8") as readme:
             readme.write(f"Resultado del test, Fecha: ${datetime.now()}")
             readme.write(f"❌ Tests fallidos")
     else:
