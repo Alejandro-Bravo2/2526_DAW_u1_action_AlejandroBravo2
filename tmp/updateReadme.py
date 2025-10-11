a4a12513111c30eff64c2b8ae3a9d64b58269223
Alejandro-Bravo2 <abravoc@iesrafaelalberti.es>
2025-10-11 17:10:43 +0200

    resolviendo problemas test readme

--- tmp/updateReadme.py
+++ tmp/updateReadme.py
@@ -5,7 +5,7 @@ with open("../../tmp/salidaEjecucion.txt", "r", encoding="utf-8") as archivo:
     lineas = contenido.split("\n")
     if ("> Task :test FAILED" in lineas):
         with open("../../README.md", "a", encoding="utf-8") as readme:
-            readme.write(f"## Resultado del test, Fecha: {datetime.now()}")
+            readme.write(f"## Resultado del test, Fecha: {datetime.now()}\n")
             readme.write(f"❌ Tests fallidos\n")
     else:
         with open("../../README.md", "a", encoding="utf-8") as readme:
